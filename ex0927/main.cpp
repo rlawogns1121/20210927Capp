4f5da38d3171dd7c0f1b6c4ae79cbec5a2037220
JaeHoonKim <rlawogns1121@gmail.com>
2021-09-27 10:38:54 +0900

    main.coo hello

--- ex0927/main.cpp
+++ ex0927/main.cpp
@@ -1,5 +1,31 @@
 #include <stdio.h>
-int main(void)
+
+void PrintInteger(int n)
 {
 
+		for (int i = 0; i < n; i++)
+			printf("Hello: %d\n", i);
+	
 }
+int main()
+{
+	PrintInteger(100);
+}
+
+//#include <stdio.h>
+//int main()
+//{
+//	for (int i = 0; i < 10; i++)
+//		printf("Hello: %d\n", i);
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	printf("Hello\n");
+//	printf("Hello\n");
+//	printf("Hello\n");
+//	printf("Hello\n");
+//	printf("Hello\n");
+//
+//}
